e370a0fcea38e3a547b3a2d2de18af2cad9ad2aa
Saikumar075 <bommenasaikumar6@gmail.com>
2026-04-20 18:20:05 +0530

    kkjjhjhn

--- task2.py
+++ task2.py
@@ -2,6 +2,6 @@ print("hello")
 print("bharathi")
 print("akhill+dashbord openingssshhhhnmnnjhg")
 ###janardhan code
-print("with in the same file task2 ,i added ggg")
-print("sai")
+print("with in the same file task2 ,i added gggughjghjhghjggh")
+print("saikjhkj")
 
