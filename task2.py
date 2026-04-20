4d097a56aba7df73b1e5e1f466ea37881edf2ebb
Saikumar075 <bommenasaikumar6@gmail.com>
2026-04-20 17:23:45 +0530

    ttt

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 print("hello")
 print("bharathi")
-print("akhill+dashbord openingsss")
+print("akhill+dashbord openingssshhhh")
 ###janardhan code
 print("with in the same file task2 ,i added some code")
 
