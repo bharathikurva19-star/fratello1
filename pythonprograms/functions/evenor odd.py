de7ed14d024fd76f0dcdda7623e8970b9c6bfb4c
bharathi <bharathikurva19@gmail.com>
2026-04-23 12:13:10 +0530

    even and odd(functions)

---
+++ pythonprograms/functions/evenor odd.py
@@ -0,0 +1,11 @@
+#EVEN_OR_ODD
+def isevenorOdd(n):
+    # n=int(input("enter the value"))
+    if n%2==0:
+        print("even")
+
+    else:
+        print("odd")
+n=int(input("enter the value"))
+isevenorOdd(n)
+# n = int(input("enter the value"))
